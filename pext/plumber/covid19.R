4fafc799dfe2bd8c83174ef94abfa290d0aeea58
Valcarcel Salamanca <Beatriz.ValcarcelSalamanca@fhi.no>
2021-04-16 13:06:57 +0200

    fix NA

--- pext/plumber/covid19.R
+++ pext/plumber/covid19.R
@@ -574,7 +574,6 @@ function(req, res, api_key, prelim=FALSE, lang="nb", location_code){
 
   d<- d[date>="2020-04-01"]
   d[is.na(pr100_pos), pr100_pos:=0]
-  d[pr100_pos==NA, pr100_pos:=0]
 
   if(lang=="nb"){
     setnames(d, c(
